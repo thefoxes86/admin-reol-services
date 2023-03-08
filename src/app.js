85ae755d36668d7bd0c4cd9fca0b2cb8b58e0428
thefoxes86 <nicola.volpi86@gmail.com>
2023-03-08 17:28:05 +0100

    Implement navbar and dataTable with mock

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { data } from './api/mock'
+
 //Sidebar
 ;(function () {
   'use strict'
@@ -8,3 +10,28 @@
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+window.addEventListener('load', () => {
+  const table = $('#example').DataTable({
+    pageLength: 20,
+    data: data,
+    columns: [
+      { data: 'data' },
+      { data: 'nome' },
+      { data: 'codice' },
+      { data: 'comune' },
+      { data: 'indirizzo' },
+      { data: 'prov' },
+      { data: 'icon1' },
+      { data: 'icon2' },
+      { data: 'icon3' },
+      { data: 'icon4' },
+      { data: 'icon5' },
+      { data: 'icon6' },
+      { data: 'icon7' },
+      { data: 'icon8' },
+    ],
+  })
+
+  table.columns.adjust.draw()
+})
